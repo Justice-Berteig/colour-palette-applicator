13a6c2bb7cffa1c1c4d29eab8e07e61ea9ec40ac
Justice Berteig <jberteig@gmail.com>
2026-03-03 12:08:49 -0500

    Started work on changing how palettes are loaded
    Used to have all palettes in a single .json file and colour maps were stored with the config files they were mapping to. Now each palette is a separate .json file which also contains its colour maps. This will let each palette map its colours differently.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import os
+import re
 
 from targets.btop     import apply_colours_to_btop
 from targets.foot     import apply_colours_to_foot
@@ -7,71 +9,63 @@ from targets.hyprland import apply_colours_to_hyprland
 from targets.nvim     import apply_colours_to_nvim
 from targets.waybar   import apply_colours_to_waybar
 
-PALETTES_FILE_NAME = "palettes.json"
-
-
-def choose_palette(palettes):
-    # Get list of palette names
-    palette_names = list(palettes.keys())
-    if len(palette_names) < 1:
-        print("Error: " + PALETTES_FILE_NAME + " contained no colour palettes!")
-        return {}
-
-    # Choose a colour palette
-    chosen_palette = {}
-    if len(palette_names) == 1:
-        # If there is only one palette choose that one
-        chosen_palette = palettes[palette_names[0]]
-    else:
-        # Otherwise ask the user which palette to use
-        print("Choose a colour palette from " + str(palette_names) + ":")
-
-        chosen_palette_name = None
-        while not chosen_palette_name in palette_names:
-            if chosen_palette_name is not None:
-                print(
-                    "Invalid selection! Please choose from "
-                    + str(palette_names)
-                    + ":"
-                )
-            chosen_palette_name = input()
-        print()
-
-        chosen_palette = palettes[chosen_palette_name]
-
-        # Print the chosen palette
-        print(
-            "You chose \'"
-            + chosen_palette_name
-            + "\' which contains the colours:"
-        );
-        for key, value in chosen_palette.items():
-            print("{: <13}: {: <7}".format(key, value))
-        print()
-
-    return chosen_palette
-
 
 def main():
-    # Read the palettes from JSON
-    palettes = {}
     try:
-        with open(PALETTES_FILE_NAME) as infile:
-            palettes = json.load(infile)
+
+        # Get list of palette names
+        files = os.listdir("./palettes/")
+        palettes = []
+        for file in files:
+            if file.endswith(".json"):
+                palettes.append(re.sub(r"\.json$", "", file))
+        print(palettes)
+
+        # Select a colour palette
+        selected_palette = None
+
+        if len(palettes) == 0:
+            raise RuntimeError("Error: No palettes in ./palettes/")
+        elif len(palettes) == 1:
+            selected_palette = palettes[0]
+        else:
+            print("Select a colour palette to apply:")
+            for index, palette in enumerate(palettes):
+                print("[" + str(index + 1) + "] " + palette)
+
+            while not selected_palette in palettes:
+                if selected_palette is not None:
+                    print("Invalid selection! Try again:")
+                user_input = input()
+                if (
+                    re.match(r"^[0-9]+$", user_input)
+                    and int(user_input) <= len(palettes)
+                ):
+                    selected_palette = palettes[int(user_input) - 1]
+                else:
+                    selected_palette = user_input
+            print()
+
+        print("Applying " + selected_palette + " palette...")
+
+        # Load palette
+        palette = None
+        with open("./palettes/" + selected_palette + ".json") as palette_file:
+            palette = json.load(palette_file)
+
+        colours = palette["colours"]
+
+        # Apply palette to targets
+        apply_colours_to_btop(colours)
+        apply_colours_to_foot(colours)
+        apply_colours_to_fuzzel(colours)
+        apply_colours_to_hyprland(colours)
+        apply_colours_to_nvim(colours)
+        apply_colours_to_waybar(colours)
+
     except Exception as e:
         print(e)
         return
-
-    # Choose a colour palette from the palettes
-    palette = choose_palette(palettes)
-
-    # Apply colours to all targets
-    apply_colours_to_btop(palette)
-    apply_colours_to_foot(palette)
-    apply_colours_to_fuzzel(palette)
-    apply_colours_to_hyprland(palette)
-    apply_colours_to_nvim(palette)
-    apply_colours_to_waybar(palette)
 
 
 if __name__ == "__main__":
